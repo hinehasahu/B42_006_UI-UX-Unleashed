2416bcab3b120f29a53115aa1e9b578ea3c2a368
AdityaDixit-2000 <adityadixitp123@gmail.com>
2025-02-07 18:16:40 +0530

    bug-fix

--- nav.js
+++ nav.js
@@ -4,8 +4,24 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Example: Toggle Account Dropdown (Dummy Implementation)
     document.querySelector('.account').addEventListener('click', function() {
-        alert("Account options will be shown here.");
+        // alert("Account options will be shown here.");
+        document.querySelector('.account>div').style.display = 
+            document.querySelector('.account>div').style.display == 'block' ? 'none' : 'block';
     });
+    let register = document.querySelector('.account>div');
+    console.log(register);
+    register.addEventListener('click', (e) => {
+        if(e.target.textContent == 'Register'){
+            // alert('register..');
+            window.location.href = './myAccount/registerPage/register.html';
+            
+        }
+        
+        if(e.target.textContent == 'Login'){
+            // alert('login..');
+            window.location.href = './myAccount/loginPage/login.html';
+        }
+    })
 
     // Example: Cart Alert
     document.querySelector('.cart-summary button').addEventListener('click', function() {
